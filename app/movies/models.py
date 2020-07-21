d7f6c2e975535591546b004b7fe340a5258f9db6
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-07-21 07:59:27 +0200

    add model movie and tests

--- app/movies/models.py
+++ app/movies/models.py
@@ -20,3 +20,14 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    year = models.CharField(max_length=4)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
