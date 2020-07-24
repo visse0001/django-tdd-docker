29eb62261a244a7246bc4df075207192666ec516
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-07-24 10:01:42 +0200

    add serializers

--- app/movies/serializers.py
+++ app/movies/serializers.py
@@ -7,4 +7,4 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
-        read_only_fields = ('id', 'created_date', 'updated_date',)
+        read_only_fields = ('id', 'cre]ated_date', 'updated_date',)
