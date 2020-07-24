1420a793dec0701f4de41b4d6a91388c67605005
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-07-24 15:17:24 +0200

    add view for movie detail

--- app/movies/views.py
+++ app/movies/views.py
@@ -14,3 +14,16 @@ class MovieList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MovieDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
